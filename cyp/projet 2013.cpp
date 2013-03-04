2917e0b4ec2f27ae0d0cf72d425a4e7642d38f44
legendaire45 <jimmy.furet@gmail.com>
2013-03-04 15:31:35 +0100

    ajout lecture fichier

--- cyp/projet 2013.cpp
+++ cyp/projet 2013.cpp
@@ -3,9 +3,32 @@
 #include <stdio.h>
 #include <iostream>
 #include "prj.h"
+#include <string>
 using namespace std;
 
-int main
+void getLineInFile (const char *filename)
+ {
+	// Reads the text file and prints each line of this file
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	fp = fopen (filename, "r"); // open the file
+	if (fp == NULL)
+		throw (" ERROR ");
+	else 
+	{
+		while ((read = getline(&line, &len, fp)) != -1) 
+		{
+		cout << "Line : " << line;
+		}
+	if (line) delete(line);
+	fclose(fp);
+	}
+}
+
+int main()
 {
-	
+	getLineInFile("texte.txt");
+
 }
