280e04ca8e5747d7a70c7efcc2f483a9a2ba5155
legendaire45 <jimmy.furet@gmail.com>
2013-03-04 15:29:03 +0100

    ajout des structure

--- cyp/projet 2013.cpp
+++ cyp/projet 2013.cpp
@@ -3,7 +3,29 @@
 #include <stdio.h>
 #include <iostream>
 #include "prj.h"
+#include <string>
 using namespace std;
+
+struct listeMots
+{
+	string mot;
+	listeMots* lien;
+};
+
+struct place
+{
+	int endroit;
+	place* lien;
+};
+
+typedef place* lieux;
+typedef	listeMots* words;
+
+words rendreVide()
+{
+	words liste = NULL;
+	return liste;
+}
 
 int main()
 {
