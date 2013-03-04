f8cf93f26bd7226478800ad8233f15612a5beb62
legendaire45 <jimmy.furet@gmail.com>
2013-03-04 16:23:34 +0100

    ajout des fonction principal

--- cyp/projet 2013.cpp
+++ cyp/projet 2013.cpp
@@ -6,12 +6,6 @@
 #include <string>
 using namespace std;
 
-struct listeMots
-{
-	string mot;
-	listeMots* lien;
-};
-
 struct place
 {
 	int endroit;
@@ -19,15 +13,72 @@ struct place
 };
 
 typedef place* lieux;
+
+struct listeMots
+{
+	string mot;
+	lieux line;
+	listeMots* lien;
+};
+
 typedef	listeMots* words;
 
-words rendreVide()
+words CreateIndex()
 {
-	words liste = NULL;
-	return liste;
+	words index = NULL;
+	return index;
+}
+
+void afficherIndex(words tIndex)
+{
+	if(tIndex != NULL)
+	{
+		cout<<tIndex -> mot<<":";
+		if (tIndex -> line != NULL)
+		{
+			cout<<tIndex -> line.endroit;
+		}
+		cout<<endl;
+	}
+}
+
+void buildKeyWordIndex (words tIndex)
+{
+	tIndex = new listeMots;
+	cout<<"Quel mot désirez vous chercher?"<<endl;
+	cin>>tIndex -> mot;
+	tIndex -> line = NULL;
+	tIndex -> lien = NULL;
+	cout<<"Desirez vous chercher un autre mot?(o/n)  ";
+	char c;
+	cin >> c;
+	if(c == 'o' || c == 'O')
+	{
+		buildKeyWordIndex(tIndex -> lien);
+	}
+}
+
+void searchKeyWordsInsertLines(words tIndex)
+{
+	FILE* f = fopen("test.txt","wb");
+	fclose(f);
+
+}
+
+void visit()
+{
+
 }
 
 int main()
 {
-	
+	//Création de la structure pour l’index
+	words tIndex = CreateIndex ();
+	//Construire l’index avec les mots clés.
+	buildKeyWordIndex (tIndex);
+	//Compléter l’index avec les lignes par rapporté un fichier donné
+	searchKeyWordsInsertLines(tIndex);
+	//Affichage et génération d’un fichier avec le résultat.
+	visit ();
+	afficherIndex(tIndex);
 }
